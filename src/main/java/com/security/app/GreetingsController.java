3f93e692a163b83f100dbcee860c9ff032fac659
Sougata2006 <psougata32@gmail.com>
2026-08-23 00:39:45 +0530

    chore: refine Spring Security configuration

--- src/main/java/com/security/app/GreetingsController.java
+++ src/main/java/com/security/app/GreetingsController.java
@@ -12,10 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.AuthenticationException;
 
 import java.util.HashMap;
@@ -24,6 +21,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/api")
 public class GreetingsController {
 
     @Autowired
--- src/main/java/com/security/app/SecurityConfig.java
+++ src/main/java/com/security/app/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.security.app;
 
+import com.security.app.jwt.AuthEntryPointJwt;
+import com.security.app.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,13 +16,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -28,9 +30,19 @@ public class SecurityConfig {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((requests) ->requests.requestMatchers("/h2-console/**").permitAll()
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**")
+                .permitAll()
+                .requestMatchers("/api/signin").permitAll()
                 .anyRequest().authenticated());
 
         http.csrf(csrf ->
@@ -45,14 +57,20 @@ public class SecurityConfig {
         http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http.httpBasic(withDefaults());
+        http.exceptionHandling(e -> e.authenticationEntryPoint(unauthorizedHandler));
+
+//        http.httpBasic(withDefaults());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
 
-        UserDetails user1 = User.withUsername("user1").password(passwordEncoder().encode("password1")).roles("USER").build(); //{noop} is not a production grade practice after i will build password encoders
+        UserDetails user1 = User.withUsername("user1").password(passwordEncoder().encode("password1")).roles("USER").build(); //{noop} is not a production grade practice after I will build password encoders
         UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN").build();
 
 
@@ -67,7 +85,12 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) {
+        return builder.getAuthenticationManager();
     }
 }
