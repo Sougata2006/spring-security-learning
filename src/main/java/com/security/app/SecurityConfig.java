a15141372a4522cd40301cd836ff49a38199556d
Sougata2006 <psougata32@gmail.com>
2026-08-22 19:39:12 +0530

    feat: implement H2 database-backed authentication

--- src/main/java/com/security/app/SecurityConfig.java
+++ src/main/java/com/security/app/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.security.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -10,13 +11,20 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
@@ -46,6 +54,13 @@ public class SecurityConfig {
         UserDetails admin = User.withUsername("admin").password("{noop}adminPass").roles("ADMIN").build();
 
 
-        return new InMemoryUserDetailsManager(user1, admin);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+
+        return userDetailsManager;
+
+//        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
