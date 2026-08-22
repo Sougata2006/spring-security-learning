52fbe54a017397ffc87872fe2dd7e29a74e4e998
Sougata2006 <psougata32@gmail.com>
2026-08-22 18:58:38 +0530

    feat: enforce role-based access control for APIs

--- src/main/java/com/security/app/GreetingsController.java
+++ src/main/java/com/security/app/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.security.app;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello !!";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint(){
+        return "Hello user!!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint(){
+        return "Hello admin!!";
     }
 }
